862463ed4438f5d188a44ab3b121b72de83fe91a
DarviL <davidlosantos89@gmail.com>
2023-11-26 17:14:14 +0100

    Initial commit

---
+++ src/main/java/Pair.java
@@ -0,0 +1,6 @@
+/**
+ * A record that stores two objects.
+ * @param <TFirst> The type of the first object.
+ * @param <TSecond> The type of the second object.
+ */
+public record Pair<TFirst, TSecond>(TFirst first, TSecond second) {}
