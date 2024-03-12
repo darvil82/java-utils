75a54823ba2ee871c2a775aa67886587f475ff5d
darvil82 <48654552+darvil82@users.noreply.github.com>
2024-03-12 11:22:29 +0000

    Deploying to javadoc from @ darvil82/java-utils@a860b6c9bf923c7556dbce3b25d9d5cde9b7ee38 🚀

--- package-search-index.js
+++ package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"utils","l":"utils"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"utils","l":"utils"},{"m":"utils","l":"utils.exceptions"}];updateSearchResults();
